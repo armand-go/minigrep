4d6b7c0bf52b1f7c6a0d5d691a868cee802f3bbc
Armand G <armdgnthr@gmail.com>
2022-10-04 22:35:49 +0200

    Set Standard Error message instead of Standard Input

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {err}");
+        eprintln!("Problem parsing argument: {err}");
         process::exit(1);
     });
 
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
